606559b75278848c6ee58773a60124d50d776812
Chris Loew <cloew123@gmail.com>
2018-07-15 17:33:53 -0600

    Added boiler plate setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='kanji_stroke_scraper',
+      version='0.0.00',
+      #description='Kao Tessur Deck Package',
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      packages=['kanji_stroke_scraper'],
+      # scripts=['kao_project/scripts/kproj']
+     )
