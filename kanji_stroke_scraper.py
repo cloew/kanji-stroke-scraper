5b0fb1b2d288401edfb8d9fc23e22ee6d3438862
Chris Loew <cloew123@gmail.com>
2018-07-15 18:49:07 -0600

    Updated the Kanji Stroke Scraper to run the Javascript to populate the svg

--- kanji_stroke_scraper.py
+++ kanji_stroke_scraper.py
@@ -1,14 +1,29 @@
 import sys
 import urllib.request
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+from lxml.etree import tostring
 
 TEST_URL = "https://jisho.org/search/%E5%AE%B6%20%23kanji"
+SVG_SELECTOR = ".stroke_order_diagram--outer_container svg"
+
+def extract_svg(pageHtml):
+    """ Extract the SVG from the contents """ 
+    session = HTMLSession()
+    r = session.get(TEST_URL)
+    r.html.render()
+    svg = pageHtml.find(SVG_SELECTOR, first=True)
+    lxmlElement = svg.lxml[0] # the lxml element actually has html as the root element rather than the svg, so grab the first child
+    return tostring(lxmlElement, pretty_print=True).decode()
 
 def main(args):
     """ Scrape for the SVG Stroke Order Diagram for a given Kanji """
-    with urllib.request.urlopen(TEST_URL) as f:
-        contents = f.read().decode()
-    print(contents)
-    print(len(contents.split('\n')))
+    session = HTMLSession()
+    page = session.get(TEST_URL)
+    page.html.render()
+    
+    svg = extract_svg(page.html)
+    print(svg)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
