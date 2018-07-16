67ca416d487cf9bc1bcff26fe17fb0abf751df4b
Chris Loew <cloew123@gmail.com>
2018-07-15 20:17:02 -0600

    Added dependencies to setup script

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='kanji_stroke_scraper',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
       py_modules=['kanji_stroke_scraper'],
+      install_requires=['backoff', 'pyperclip', 'requests-html'],
       scripts=['scripts/scrapekanjidiagram']
      )
